ac3a602ae7be761f70ac70639abe4bcb49b4bc6e
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-09-29 11:18:03 -0500

    (feat) Top Nav Bar renders links to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,35 +7,52 @@ import {
 } from "react-router-dom";
 import About from './About.jsx';
 import Portfolio from './Portfolio.jsx';
+import Home from './Home.jsx';
+import { Tabs, Tab } from '@material-ui/core';
 
 function App() {
+  const allLinks = ['/', '/about', '/portfolio'];
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/portfolio">Portfolio</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/">
-            {/* <Home /> */}
-          </Route>
-        </Switch>
+        <Route 
+          path="/"
+          render={({ location }) => (
+            <Fragment>
+              <Tabs value={location.pathname}>
+                <Tab 
+                label="Home" 
+                value="/"
+                component={Link}
+                to={allLinks[0]}
+                />
+                <Tab 
+                label="About" 
+                value="/about"
+                component={Link}
+                to={allLinks[1]}
+                />
+                <Tab 
+                label="Portfolio" 
+                value="/portfolio"
+                component={Link}
+                to={allLinks[2]}
+                />
+              </Tabs>
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/portfolio">
+                  <Portfolio />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch> 
+            </Fragment>
+          )}
+        />
       </div>
     </Router>
   );
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function Portfolio(){
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      Welcome to my Page!
+    </div>
+  );
+}
+
+export default Portfolio;
