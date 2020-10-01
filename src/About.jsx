23c58bafe9961235f81407f8195a073ca31d25b6
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-10-01 18:22:48 -0500

    (feat) added 'about me' basics with some style

--- src/About.jsx
+++ src/About.jsx
@@ -1,16 +1,45 @@
 import React, { useState } from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 function About(){
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#00194E',
+      },
+      secondary: {
+        main: '#fc9303',
+      },
+    },
+  });
 
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
+    <ThemeProvider theme={theme}>
+
+    <Container 
+    maxWidth="sm"
+    style={{backgroundColor: '#0f4c81'}}
+    >
+        <Typography 
+        variant="h4"
+        style={{color: "#ccd6f6"}}
+        >
+          About Me
+
+        </Typography>
+
+        <Typography 
+        paragraph
+        style={{color: "#ccd6f6"}}
+        >
+          Problem solving and analysis is my passion. For the last several years I have been studying and, more recently, teaching chess, a game which demands calculation ability and analytical skill. 
+          Software Development has allowed me to realize this love in a new light, as I am faced with new and exciting problems on a daily basis. 
+          Whether it be cracking an algorithm or scouring documentation for understanding of new technology, I am always willing to put myself to the test!
+
+        </Typography>
+    </Container>
+    </ThemeProvider>
   );
 }
 
