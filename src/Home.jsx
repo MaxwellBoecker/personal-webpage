16015d9281be069b46e59e8bd00a44b4bea7f73c
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-10-01 18:24:26 -0500

    (feat) displayed and modified image on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,31 @@
+import { Typography, Container } from '@material-ui/core';
+import { borderRadius } from '@material-ui/system'
 import React, { useState } from 'react';
+import headshot from './headshot.jpeg'
 
 function Portfolio(){
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
 
   return (
     <div>
-      Welcome to my Page!
+      <Container maxWidth="sm">
+        <Typography 
+        variant="h3"
+        style={{color: "#ccd6f6"}}
+        >
+        Welcome to My Page!
+
+        </Typography>
+        <img src={headshot} 
+        alt="Headshot" 
+        style={{
+          borderRadius: '50%',
+          maxWidth: '500px',
+          maxHeight: '500px'
+
+        }}
+        />
+
+      </Container>
     </div>
   );
 }
