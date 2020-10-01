2e123b746a6136cfab0f4a424080fb3e34ca1d35
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-10-01 18:25:04 -0500

    (feat) added resume and sidebar files, made placeholders in portfolio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,15 +1,19 @@
+import { Container, Typography } from '@material-ui/core';
 import React, { useState } from 'react';
 
 function Portfolio(){
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <Container maxWidth="md">
+      <Typography variant="h4">
+        Horter
+      </Typography>
+      <Typography variant="h4">
+        Harbinger
+      </Typography>
+      </Container>
     </div>
   );
 }
