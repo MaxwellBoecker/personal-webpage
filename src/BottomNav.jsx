e65216f56cf58ee759e45f1854a8c017494b304a
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-10-03 13:07:16 -0500

    (feat) added bottom nav

---
+++ src/BottomNav.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BottomNavigation, Typography } from '@material-ui/core';
+
+function BottomNav(){
+  return(
+    <BottomNavigation
+    style={{background: '#484e4a'}}
+    >
+      <Typography
+      style={{color: '#eacb48'}}
+      >
+        icons go here
+      </Typography>
+    </BottomNavigation>
+  )
+  
+}
+
+export default BottomNav;
