4ea0713fed5685a4664f7d2104150e1f48ff6813
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-10-06 14:01:59 -0500

    (refactor) used footer component for BottomNav. added icon buttons and copyrght

--- src/BottomNav.jsx
+++ src/BottomNav.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import { BottomNavigation, Typography } from '@material-ui/core';
+import { IconButton, Typography } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { FaGithub, FaLinkedin } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+//import { FaGithub } from "react-icons/fa";
 
 function BottomNav(){
+  console.log(faGithub);
+  const githubIcon = <FontAwesomeIcon icon={faGithub} size='lg'/>
+  const linkedInIcon = <FontAwesomeIcon icon={faLinkedin} size='lg'/>
   return(
-    <BottomNavigation
+    <footer
     style={{
-      background: '#484e4a',
+      background: 'transparent',
       width: '100%',
       // position: 'fixed',
     }}
     
     >
-      {/* <Typography
-      style={{color: '#eacb48'}}
-      >
-        icons go here
-      </Typography> */}
-    </BottomNavigation>
+      <Typography variant='h3' align='center'>
+        <IconButton children={githubIcon} style={{color: "black"}} href={'https://www.github.com/MaxwellBoecker'} target='_blank'/>
+
+        <IconButton children={linkedInIcon} style={{color: "black"}} href={'https://www.linkedin.com/in/maxwell-boecker-61b6191b6/'} target='_blank'/>
+
+            
+       
+
+      </Typography>
+      <br></br>
+      <Typography paragraph align='center'>
+            © 2020 Maxwell Boecker
+      </Typography>
+    </footer>
   )
   
 }
