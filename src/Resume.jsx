705a62fe6121e6a2699f63273ea7261c0671aaf0
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-11-30 14:55:19 -0600

    (feat) added updated version of resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import { Document, Page, pdfjs } from 'react-pdf';
-import resume from './assets/Resume Maxwell Boecker (2).pdf';
+// import resume from './assets/Resume Maxwell Boecker (2).pdf';
+import resume from './assets/Maxwell Boecker Resume Isometric.pdf';
 
 function Resume() {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
